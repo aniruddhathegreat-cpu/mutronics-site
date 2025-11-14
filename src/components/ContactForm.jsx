635acc0829f1d2c5efebdeea19e7dc2a85ed4b4d
aniruddhathegreat-cpu <aniruddhathegreat@gmail.com>
2025-11-14 18:57:11 +0530

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 // -------------------------------------------------------------
 // CONFIG — UPDATE THESE:
 // -------------------------------------------------------------
-const RECAPTCHA_SITE_KEY = "6LcXcwwsAAAAACVbz0Jlm19sM9KtHbHFOzFk0fgG";
+const RECAPTCHA_SITE_KEY = "6LfQjgwsAAAAAEdurayKaqfWOnaXdVEyBMAqO3ay";
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby3DoTdsrW63TncOnIYQ7Lk6a07KCwY_8ooY10EDXIKysyyU6JDoYgszGrzGgEIBPZF/exec";
 // -------------------------------------------------------------
 
